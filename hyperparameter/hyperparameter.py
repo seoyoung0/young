e0c5c8fe0a84577947fb9d2e2f86b013afbc1445
seoyoung0 <80498213+seoyoung0@users.noreply.github.com>
2021-05-06 20:06:05 +0900

    Add files via upload

---
+++ hyperparameter/hyperparameter.py
@@ -0,0 +1,11 @@
+episodeNumber = 10000
+stepNumber = 1000
+gamma = 0.95
+learningRate = 0.001
+layerNode = 64
+
+epsilon = 1.5
+epsilonDecay = 0.9995
+epsilonMin = 0.03
+bufferSize = 2048
+replaySize = 64
